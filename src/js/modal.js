80bd48b5125ad5ae9cea530ec8d1f5e45f330aba
Vania_Dmitriienko <dmitrienko2706@gmail.com>
2022-06-11 19:10:06 +0300

    Add modal-btn

---
+++ src/js/modal.js
@@ -0,0 +1,16 @@
+// (() => {
+// const refs = {
+//     openMenuBtn: document.querySelector(".menu-open-btn"),
+//     closeMenuBtn: document.querySelector(".modal__close-btn"),
+//     menu: document.querySelector(".modal"),
+//     body: document.querySelector('body')
+// };
+
+// refs.openMenuBtn.addEventListener("click", toggleMenu);
+// refs.closeMenuBtn.addEventListener("click", toggleMenu);
+
+// function toggleMenu() {
+//     refs.menu.classList.toggle("is-hidden");
+//     refs.body.classList.toggle('no-scroll')
+// }
+// })();
