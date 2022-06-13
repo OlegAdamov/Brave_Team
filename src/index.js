bbd897b8269c0b6712b3bb56eadc390a53a40ed4
Oleg Adamov <103196379+OlegAdamov@users.noreply.github.com>
2022-06-13 18:02:19 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -45,5 +45,13 @@
             top = $(id).offset().top;
         $('body,html').animate({scrollTop: top}, 1500);
     });
+     });
+   $(document).ready(function(){
+    $("#top").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 900);
+    });
 });
 })();
